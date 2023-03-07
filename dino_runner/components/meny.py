11360a2feb4c56e73b25e3bca59416c002bc6238
M4rt1nB <Martin.Martin@fundacion-jala.org>
2023-03-07 17:08:09 -0500

    clase_si

---
+++ dino_runner/components/meny.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class menu:
+ def __init__(self, screen):
+  screen.fill((255,255,255))
+  self.font = pygame.font.Font()
+
+ def updated(self):
+   pass
+
+ def updated(self, screen):
+  pass
